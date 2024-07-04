696cb0512b3618460a1ab8b5175bc1973f322af3
sOkam <7308253+heysokam@users.noreply.github.com>
2024-07-04 19:21:31 +0200

    chg: nvim: Better `git` tools

--- nvim/lua/plug/git.lua
+++ nvim/lua/plug/git.lua
@@ -1,25 +1,46 @@
 return {
-  "kdheepak/lazygit.nvim",
-  cmd = {
-    "LazyGit",
-    "LazyGitConfig",
-    "LazyGitCurrentFile",
-    "LazyGitFilter",
-    "LazyGitFilterCurrentFile",
+  {"kdheepak/lazygit.nvim",
+    cmd = {
+      "LazyGit",
+      "LazyGitConfig",
+      "LazyGitCurrentFile",
+      "LazyGitFilter",
+      "LazyGitFilterCurrentFile",
+    },
+    -- optional for floating window border decoration
+    dependencies = {
+      "nvim-telescope/telescope.nvim",
+      "nvim-lua/plenary.nvim",
+    },
+    config = function()
+      require("telescope").load_extension("lazygit")
+      vim.g.lazygit_floating_window_scaling_factor = 1.00
+    end,
+    -- setting the keybinding for LazyGit with 'keys' is recommended in
+    -- order to load the plugin when the command is run for the first time
+    keys = {
+      { "<leader>gg", "<cmd>LazyGitCurrentFile<cr>", desc = "LazyGit" },
+      { "<leader>gf", "<cmd>LazyGitFilterCurrentFile<cr>", desc = "LazyGit" },
+      { "<leader>gF", "<cmd>LazyGitFilter<cr>", desc = "LazyGit" },
+    }
   },
-  -- optional for floating window border decoration
-  dependencies = {
-    "nvim-telescope/telescope.nvim",
-    "nvim-lua/plenary.nvim",
+
+  -- Here is a more advanced example where we pass configuration
+  -- options to `gitsigns.nvim`. This is equivalent to the following Lua:
+  --    require('gitsigns').setup({ ... })
+  --
+  -- See `:help gitsigns` to understand what the configuration keys do
+  { -- Adds git related signs to the gutter, as well as utilities for managing changes
+    "lewis6991/gitsigns.nvim",
+    opts = {
+      signs = {
+        change       = { text = "│" },
+        add          = { text = "┃" },
+        delete       = { text = "┗" },
+        topdelete    = { text = "‾" },
+        changedelete = { text = "~" },
+      },
+    },
   },
-  config = function()
-    require("telescope").load_extension("lazygit")
-  end,
-  -- setting the keybinding for LazyGit with 'keys' is recommended in
-  -- order to load the plugin when the command is run for the first time
-  keys = {
-    { "<leader>gg", "<cmd>LazyGitCurrentFile<cr>", desc = "LazyGit" },
-    { "<leader>gf", "<cmd>LazyGitFilterCurrentFile<cr>", desc = "LazyGit" },
-    { "<leader>gF", "<cmd>LazyGitFilter<cr>", desc = "LazyGit" },
-  }
 }
+
